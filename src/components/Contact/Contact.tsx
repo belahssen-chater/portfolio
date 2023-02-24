046846afda1f686592fc6beed7822d76ee15860e
CHATER <belahssen.chater@itbs.tn>
2023-02-24 22:50:54 +0100

    test

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,28 +1,31 @@
 import { Container } from "./styles";
 import emailIcon from "../../assets/email-icon.svg";
-import phoneIcon from "../../assets/phone-icon.svg"
+import phoneIcon from "../../assets/phone-icon.svg";
 import { Form } from "../Form/Form";
 
-
-export function Contact(){
-
-  return(
+export function Contact() {
+  return (
     <Container id="contact">
       <header>
         <h2>Contact me</h2>
-        <p>If you've seen my potential or want to talk to me, don't hesitate to send me a message.</p>
+        <p>
+          If you've seen my potential or want to talk to me, don't hesitate to
+          send me a message.
+        </p>
       </header>
       <div className="contacts">
         <div>
           <img src={emailIcon} alt="Email" />
-          <a href="mailto:belahssenchater1@gmail.com">belahssenchater1@gmail.com</a>
+          <a href="mailto:belahssenchater1@gmail.com">
+            belahssenchater1@gmail.com
+          </a>
         </div>
         <div>
           <img src={phoneIcon} alt="Email" />
           <a href="tel:+21628405051">+21628405051</a>
-        </div>  
+        </div>
       </div>
       <Form></Form>
     </Container>
-  )
+  );
 }
